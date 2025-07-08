63648239b00645aa48019c8f9d7555216b1aea5d
matheus.ferreira <mhateus1995@gmail.com>
2025-07-07 23:03:58 -0300

    first commit

---
+++ src/main/java/ProjetoViasoft/ProjetoViasoft/ProjetoViasoftApplication.java
@@ -0,0 +1,13 @@
+package ProjetoViasoft.ProjetoViasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoViasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoViasoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjetoViasoft/ProjetoViasoft/ProjetoViasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoViasoft.ProjetoViasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoViasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
